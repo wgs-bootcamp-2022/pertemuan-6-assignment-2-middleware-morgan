720c41fc6d766aa3a9252561f25b1c91554d191a
zakariyahya <yazakkars@gmail.com>
2022-09-13 08:30:40 +0700

    crud and validation work

--- app.js
+++ app.js
@@ -1,82 +1,3 @@
-// const express = require("express")
-// const accountRoutes = express.Router();
-// const fs = require('fs');
-// const dataPath = './data/account.json' // path to our JSON file
-// const expressLayouts = require('express-ejs-layouts')
-
-
-
-// const getAccountData = () => {
-//     const jsonData = fs.readFileSync(dataPath)
-//     return JSON.parse(jsonData)   
-// }
-
-// const saveAccountData = (data) => {
-//     const stringifyData = JSON.stringify(data)
-//     fs.writeFileSync(dataPath, stringifyData)
-// }
-
-
-// const app = express()
-
-// app.use(expressLayouts)
-// app.set('view engine', 'ejs')
-
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: true}))
-
-// app.use('/',accountRoutes)
-
-// app.get('/', (req,res)=>{
-//     res.render('index',{
-//         name : "Yahya",
-//         // title: "web yahya",
-//         layout:'layout/home'
-
-//     })
-// })
-
-
-// accountRoutes.get('/all', (req, res) => {
-//     const accounts = getAccountData()
-//     res.send(accounts)
-// })
-
-// accountRoutes.post('/account/add', (req, res)=>{
-//     const accounts = getAccountData()
-//     const userData = req.body
-//     accounts.push(userData)
-
-//     saveAccountData(accounts)
-//     res.send({success: true, msg: 'account added successfully'})
-// })
-
-// accountRoutes.delete('/account/delete/:id', (req,res) => {
-//     const id = req.params.id
-//     const accounts = getAccountData()
-//     const filter = accounts.filter(user => user.id != id);
-//     saveAccountData(filter)
-//     res.send({success: true, msg: 'User removed successfully'})
-// })
-
-// accountRoutes.patch('/account/edit/:id', (req,res)=>{
-//     const id = req.params.id
-//     const userData = req.body
-//     console.log(userData)
-
-//     const accounts = getAccountData()
-
-//     const updateAccount = accounts.filter( user => user.id === id )
-//     updateAccount.push(userData)
-//     saveAccountData(updateAccount)
-    
-//     res.send({success: true, msg: 'User data updated successfully'})
-// })
-
-// app.listen(3000, ()=> {
-//     console.log('listening port 3000')
-// })
-// const Controller = require('./controllers/controller')
 const controller = require('./controllers/controller')
 
 const expressLayouts = require('express-ejs-layouts')
@@ -84,19 +5,17 @@ const express = require('express');
 const app = express();
 app.use(expressLayouts)
 app.set('layout', './layout/home')
-
-
 app.set('view engine', 'ejs');
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
 app.get('/', controller.getHome);
 app.get('/contact', controller.getContact)
-app.get('/contact/add', controller.addContact)
-app.post('/contact/add', controller.addPostContact)
+app.get('/contact/add',  controller.addContact)
+app.post('/contact/add', controller.isValidationAddContact)
 app.get('/contact/:id/edit', controller.updateContact)
-app.post('/contact/:id/edit', controller.updatePostContact)
+app.post('/contact/:id/edit', controller.isValidationEditContact)
 app.get('/contact/:id/delete', controller.deleteContact)
 
 const port = 3000;
-app.listen(port, console.log("Server has started at port " + port))
+app.listen(port, console.log(`Server has started at port " + ${port}`))
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,14 +1,15 @@
 let ejs = require("ejs")
 const fs = require('fs')
 const dataPath = './data/account.json' // path to our JSON file
-const validator = require('validator')
+const { body, validationResult, check } = require('express-validator'); // Express Validator
 
 const getHome = (req,res) => {
     res.render('index')
 
 }
 const getAccountData = () => {
-    const jsonData = fs.readFileSync(dataPath)
+    const jsonData = fs.readFileSync(dataPath, 'utf-8')
+    console.log(jsonData)
     return JSON.parse(jsonData)   
 }
 
@@ -25,33 +26,9 @@ const getContact = (req, res) => {
 }
 
 const addContact = (req, res) => {
-
     res.render('add')
 }
 
-const addPostContact = (req, res) => {
-    // let  msg = {}
-    let id = Math.floor(Math.random() * 100);
-    for(i=0; i<dataPath.length;i++){
-        dataPath[i].id = id 
-    }
-    const accounts = getAccountData()
-    let objparam = {
-        id: id,
-        name: req.body.name,
-        email: req.body.email,
-        number: req.body.number
-    }
-    const valNumber = validator.isMobilePhone(objparam.number,'id-ID')
-    let msg = []
-    if(valNumber==false){
-        msg.push(console.log("ok"))
-    }
-    console.log(msg)
-    accounts.push(objparam)
-    saveAccountData(accounts)
-    res.redirect('/contact')
-}
 
 const updateContact = (req, res) => {
     const accounts = getAccountData()
@@ -60,31 +37,99 @@ const updateContact = (req, res) => {
     res.render('editContact',{data})
 }
 
-const updatePostContact = (req, res) => {
-    const id = +req.params.id
-    const accounts = getAccountData()
-    let objparam = {
-        id: ids,
-        name: req.body.name,
-        email: req.body.email,
-        number: req.body.number
-      }
-    const updateAccount = accounts.filter( contact => contact.id === id )
-    updateAccount.push(objparam)
-    saveAccountData(updateAccount)
-    res.redirect('/contact')
+const isDuplicat = (name) => {
+    const contacts = getAccountData();
+    return contacts.find((contact) => contact.name === name );
 }
+
+const isValidationAddContact = [
+    [
+        body('name').custom(value => {
+            const duplicat = isDuplicat(value);
+            if(duplicat) {
+            throw Error('Contact number is already used');
+            }
+            return true;
+        }),
+        check('email', 'Email address is invalid').isEmail(),
+        check('number', 'Mobile number is invalid').isMobilePhone('id-ID') 
+    ], 
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.render('add', {
+            errors: errors.array()
+            });
+        } 
+            // code add post contact
+            const id = Math.floor(Math.random() * 1000); // membuat id random
+            for(i=0; i<dataPath.length;i++){
+                dataPath[i].id = id 
+            }
+            const accounts = getAccountData()
+            let objparam = {
+                id: id,
+                name: req.body.name,
+                email: req.body.email,
+                number: req.body.number
+              }
+            const updateAccount = accounts.filter( contact => contact.id !== id )
+            updateAccount.push(objparam)
+            saveAccountData(updateAccount)
+            res.redirect('/contact')
+        
+    }
+]
+
+const isValidationEditContact = [
+    [
+        body('name').custom((value, {req}) => {
+            const duplicat = isDuplicat(value);
+            if (value !== req.body.name && duplicat) {
+                throw new Error('Your Name is already used');
+            }
+            return true;
+        }),
+        check('email', 'Email address is invalid').isEmail(),
+        check('number', 'Mobile number is invalid').isMobilePhone('id-ID') 
+    ], 
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.render('editContact', {
+            errors: errors.array()
+            });
+        } else {
+            const id = +req.params.id
+            const accounts = getAccountData()
+            let objparam = {
+                id: id,
+                name: req.body.name,
+                email: req.body.email,
+                number: req.body.number
+            }
+            const updateAccount = accounts.filter( contact => contact.id !== id )
+            updateAccount.push(objparam)
+            saveAccountData(updateAccount)
+            res.redirect('/contact')
+        }
+    }
+]
 
 const deleteContact = (req, res) => {
     const id = +req.params.id
     const accounts = getAccountData()
     const filter = accounts.filter(user => user.id != id);
     saveAccountData(filter)
-
     res.redirect('/contact')
-
 }
-module.exports = { getHome, getContact, addContact, 
-                    addPostContact, deleteContact, 
-                    updateContact, updatePostContact
+
+module.exports = {
+                    getHome, 
+                    getContact, 
+                    addContact,     
+                    deleteContact, 
+                    updateContact,      
+                    isValidationAddContact, 
+                    isValidationEditContact
                  }
